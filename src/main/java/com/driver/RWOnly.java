1c206dc30f1acccc600b5cbe0d09a805fda4f1a2
Dev <kanuj2151@gmail.com>
2023-08-17 16:20:47 +0530

    Initial commit

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
+    private String Name;
     
+    public void setName(String name){
+        this.Name = name;
+    }
+    public String getName(){
+        return this.Name;
+    }
+        
 }
