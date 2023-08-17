7b25c2bb657aaf2e996086297f0c0a5c5ed7fb81
Dev <kanuj2151@gmail.com>
2023-08-17 16:27:47 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
+
 public class Main {
+    
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        obj.setName("Ambuj");
+        System.out.println(obj.getName());
+    }
   
 }
